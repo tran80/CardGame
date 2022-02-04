6534a2ae6ba760d08c72728e20b4d8e5bb1bdb42
Trinh Tran <98617010+tran80@users.noreply.github.com>
2022-02-04 01:27:20 -0500

    Edit the code

--- src/cardgame/CardTrick.java
+++ src/cardgame/CardTrick.java
@@ -28,14 +28,30 @@ public class CardTrick {
         }
         
 
+        // Task 3 and 4
+//        System.out.print("Enter card value: ");
+//        int val = input.nextInt();
+//        System.out.print("Enter suit: ");
+//        String suit = input.next();
+//        PickCard key = new PickCard();
+//        key.setValue(val);
+//        key.setSuit(suit);
         
-        System.out.print("Enter card value: ");
-        int val = input.nextInt();
-        System.out.print("Enter suit: ");
-        String suit = input.next();
         PickCard key = new PickCard();
-        key.setValue(val);
-        key.setSuit(suit);
+        key.setSuit("Clubs");
+        key.setValue(8);
+        boolean found = false;
+        System.out.println("Searching for: " + key);
+        for(int i = 0; i < magicHand.length; i++) {
+            if((magicHand[i].getValue() == key.getValue()) && 
+                    magicHand[i].getSuit().equalsIgnoreCase(key.getSuit())) {
+                System.out.println("Card is found in magic hand at index " + (i +1));
+                found = true;
+                break;
+            }
+        }
+        if(!found)
+            System.out.println("Card is not found in magic hand");
     }
     
 }
